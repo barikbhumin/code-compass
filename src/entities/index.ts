fecee2a4fb1ab9cb6aaf6281ac88aef28665b9ed
Wix Remote Machine <support@wix.com>
2025-12-25 14:45:38 +0000

    Restored to checkpoint 8261475165831e42a83f6354f9fee69e92ceea82

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -11,6 +11,26 @@ export interface QuizQuestions {
   _id: string;
   _createdDate?: Date;
   _updatedDate?: Date;
+  /** @wixFieldType number */
+  option1Value?: number;
+  /** @wixFieldType number */
+  option5Value?: number;
+  /** @wixFieldType text */
+  option5Text?: string;
+  /** @wixFieldType number */
+  option4Value?: number;
+  /** @wixFieldType text */
+  option4Text?: string;
+  /** @wixFieldType number */
+  option3Value?: number;
+  /** @wixFieldType text */
+  option3Text?: string;
+  /** @wixFieldType number */
+  option2Value?: number;
+  /** @wixFieldType text */
+  option2Text?: string;
+  /** @wixFieldType text */
+  option1Text?: string;
   /** @wixFieldType text */
   questionText?: string;
   /** @wixFieldType text */
